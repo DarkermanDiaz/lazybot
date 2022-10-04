18fffccbcb81f7857dce25a576eb0a38defc2c99
DarkermanDiaz <67664223+DarkermanDiaz@users.noreply.github.com>
2022-10-03 19:10:28 -0500

    Mesagge edit update

--- comandos/help.js
+++ comandos/help.js
@@ -1,4 +1,16 @@
 const Discord = require("discord.js");
+let comando = 'help';
+let description = 'Display the lazybot commands!';
 module.exports = (client, message, args) => {
-  
+    const embed = new Discord.MessageEmbed()
+        .setColor('#5DADE2')
+        .setDescription('Lazybot command list')
+        .addFields(
+            { name: 'hello', value: 'Say hello to lazybot' },
+            { name: 'beauty', value: 'Evaluates someone beauty!' },
+            { name: 'conch', value: 'Ask something to the magic conch, it knows everithing!' },
+            { name: 'cookie', value: 'Give a cookie to your friend' },
+        );
+
+    message.channel.send({embed})
 };
